7f2055aa553d7a43957b45e9b31376c265ba0905
unknown <ayaz.ibrahimov.1998@mail.ru>
2022-12-27 17:45:43 +0400

    n

---
+++ js/slider.js
@@ -0,0 +1,30 @@
+$(document).ready(function(){
+    $(".header-carousel").owlCarousel({
+        items:1,
+        loop:true,
+        margin:10,
+        // autoplay:true,
+        // autoplayTimeout:3000,
+        // autoplayHoverPause:true,
+        // dots:true,
+        // nav:true,
+    });
+  });
+$(document).ready(function(){
+    $(".partner-carousel").owlCarousel({
+        items:4,
+        loop:true,
+        margin:10,
+        // autoplay:true,
+        autoplayTimeout:1500,
+        autoplayHoverPause:true,
+        // autoplayHoverPause:true,
+        // dots:true,
+        // nav:true,
+        responsive:{
+            600:{
+                items:4
+            }
+        }
+    });
+  });
