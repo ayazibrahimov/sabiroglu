56230cc9307c5732c7f206a5b8d0d9375d6b443c
unknown <ayaz.ibrahimov.1998@mail.ru>
2023-01-06 10:55:44 +0400

    s

---
+++ images/header.php
@@ -0,0 +1,144 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="description" content="BUILDY HTML5 Template" />
+
+  <title>Main page</title>
+
+  <!-- Fav Icon -->
+  <link rel="icon" type="image/x-icon" href="assets/imgs/icon/logo-3.png">
+
+  <!-- All CSS files -->
+  <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+  <link rel="stylesheet" href="assets/css/all.min.css">
+  <link rel="stylesheet" href="assets/flaticon/flaticon.css">
+  <link rel="stylesheet" href="assets/css/swiper-bundle.min.css">
+  <link rel="stylesheet" href="assets/css/progressbar.css">
+  <link rel="stylesheet" href="assets/css/animate.min.css">
+  <link rel="stylesheet" href="assets/css/meanmenu.min.css">
+  <link rel="stylesheet" href="assets/css/master.css">
+  <link rel="stylesheet" href="style.css">
+</head>
+
+
+<body>
+
+  <!-- Loading... Start -->
+  
+
+  <!-- <div class="preloader">
+    <div class="loading">
+      <div class="bar bar1"></div>
+      <div class="bar bar2"></div>
+      <div class="bar bar3"></div>
+      <div class="bar bar4"></div>
+      <div class="bar bar5"></div>
+      <div class="bar bar6"></div>
+      <div class="bar bar7"></div>
+      <div class="bar bar8"></div>
+    </div>
+  </div> -->
+
+
+  <!-- Loading... End -->
+
+
+  <!-- header area start -->
+  <header>
+
+
+    <div class="header__area">
+    
+    <div class="header__top">
+        <p>Bizimə əlaqə <span>09:30 - 10:01</span></p>
+        <span class="close-topbar"><i class="fa-solid fa-xmark"></i></span>
+      </div>
+
+      <div class="header__info">
+        <div class="container-fluid">
+          <div class="row">
+            <div class="col-xxl-6 col-xl-7 col-lg-7 col-md-6">
+              <div class="header__info-left">
+                <ul class="header__address">
+                  <li><span><i class="fa-solid fa-location-dot"></i></span>  Bakı şəhəri, Xətai rayonu, 1-ci Şərq küçəsi, 5</li>
+                  <li><a href="mailto:info@example.com"><span><i class="fa-solid fa-envelope"></i></span>info@concretebureau.az</a></li>
+                </ul>
+              </div>
+            </div>
+            <div class="col-xxl-6 col-xl-5 col-lg-5 col-md-6">
+              <div class="header__info-right">
+                <!-- <ul class="header__links">
+                  <li><a href="#">career</a></li>
+                  <li><a href="#">help</a></li>
+                </ul> -->
+                <ul class="header__social">
+                  <li><a href="https://www.facebook.com/concretebureau.az"><span><i class="fa-brands fa-facebook-f"></i></span></a></li>
+                  <!-- <li><a href="#"><span><i class="fa-brands fa-twitter"></i></span></a></li> -->
+                  <li><a href="https://www.instagram.com/concretebureau.az"><span><i class="fa-brands fa-instagram"></i></span></a></li>
+                  
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="header__menu-area" id="header_menu">
+        <div class="container-fluid">
+          <div class="row">
+            <div class="col-xxxl-3 col-xxl-3 col-xl-3 col-lg-4 col-md-4 col-5">
+            <div class="header__logo">
+                <a href="index.php">
+                  <img class="logo-1" src="assets/imgs/imgdata/concrete.png" alt="Header Logo">
+                  <img class="logo-2" src="assets/imgs/imgdata/concrete.png" alt="Header Logo">
+                </a>
+              </div>
+            </div>
+            <div class="col-xxxl-7 col-xxl-6 col-xl-6 col-lg-3 col-md-3 col-2">
+              <nav class="header__nav b-right">
+                <ul>
+                  <li><a href="index.php"><span>01</span>  Əsas səhifə</a></li>         
+                  <li><a href="about.php"> <span>02</span> Haqqımızda</a></li>
+                  <li><a href="portfolio.php"> <span>03</span> Layihələr</a>
+    
+                  </li>
+                  <li><a href="service.php"><span>04</span> Xidmətlər</a>
+                    <!-- <ul class="main-dropdown">
+                      <li><a href="service.html">service V.1</a></li>
+                      <li><a href="service-2.html">service V.2</a></li>
+                      <li><a href="service-details.html">service details</a></li>
+                    </ul> -->
+                  </li>
+                  
+                  <li><a href="contact.php"><span>05</span> Əlaqə</a></li>
+                </ul>
+              </nav>
+            </div>
+            <div class="col-xxxl-2 col-xxl-3 col-xl-3 col-lg-5 col-md-5 col-5">
+              <div class="header__others">
+                <div class="header__support">
+                  <p>Telefon<a href="tel:(+99410) 203 01 02 ">(+99410) 203 01 02</a></p>
+                </div>
+                <div class="header__lang">
+                  <button class="dropdown-toggle lang" data-bs-toggle="dropdown">AZ</button>
+                  <ul class="dropdown-menu">
+                    <li class="active">AZ</li>
+                    <li>EN</li>
+                    <li class="">RU</li>
+                  </ul>
+                </div>
+                <div class="header__offcanvas" id="open_offcanvas">
+                  <span><i class="flaticon-align-left"></i></span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+    </div>
+  </header>
